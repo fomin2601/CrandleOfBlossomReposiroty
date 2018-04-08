9f91e0e3caf59a0595c866719fec244204422419
fomin2601 <fomin260145@gmail.com>
2018-04-08 18:45:10 +0300

    First version of COB

---
+++ CrandleOfBlossom/Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+	public GameObject settings;
+	public void StartGame () {
+		Application.LoadLevel (1);
+	}
+
+	public void LoadGame () {
+
+	}
+
+	public void Options () {
+		settings.SetActive (!settings.activeSelf);
+	}
+
+	public void Quit () {
+		Application.Quit ();
+	}
+
+	public void SetMusic(float Value) {
+		Global.Music = Value;
+	}
+
+	public void SetSound(float Value) {
+		Global.Sound = Value;
+	}
+}
---
+++ CrandleOfBlossom/Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (Global.Music);
+		Debug.Log (Global.Sound);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
